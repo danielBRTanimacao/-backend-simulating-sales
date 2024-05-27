3cdff3706107e684f12b241317de71251c5a0c30
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-27 15:26:35 -0300

    new features configs new model making tests

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from project import models
 
 # Register your models here.
+@admin.register(models.ProductForSale)
+class AdminProductForSale(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'value',
+        'seller'
+    )
+    ordering = '-id',
+    search_fields = ('name', 'id', 'seller')
+    list_editable = ('value', 'seller')
+    list_per_page = 10
+    list_max_show_all = 150
+    list_display_links = 'id', 'name',
--- project/models.py
+++ project/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ProductForSale(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(default=True)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    posted_date = models.DateField(default=timezone.now)
+    img = models.ImageField(blank=True, upload_to='images/%Y/%m/')
+    seller = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.value}'
