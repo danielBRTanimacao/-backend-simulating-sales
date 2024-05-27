13bbe311f3138ba0155953af7fb85706874fb413
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-27 13:51:24 -0300

    add all configs on project created index, base.html

--- project/views.py
+++ project/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'site_title': 'Princiapal'
+    }
+    return render(request, 'project/index.html', context)
